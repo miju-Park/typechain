6dc95ab31f7cab6845209a6537ffdf56acc4ef99
WebSWTeam <uwa@WebSWTeamui-MacBook-Pro.local>
2018-07-31 10:59:53 +0900

    Types in Typescipt & Apply tsc-watch

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const name = "Nicolas",
+    age=24,
+    gender = "male";
+
+const sayHi = (name:string, age:number, gender:string):string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`
+}
+console.log(sayHi("Nicolas", 20, "male"));
+
+export {};
