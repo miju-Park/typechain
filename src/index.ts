25f9ffc83ea9c61ce7f776c53a98f228c17adf8e
WebSWTeam <uwa@WebSWTeamui-MacBook-Pro.local>
2018-07-31 11:06:07 +0900

    Interface on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-const name = "Nicolas",
-    age=24,
-    gender = "male";
-
-const sayHi = (name:string, age:number, gender:string):string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`
+interface Human {
+    name:string,
+    age: number,
+    gender: string
 }
-console.log(sayHi("Nicolas", 20, "male"));
+
+const person = {
+    name : 'nicolas',
+    age : 22,
+    gender : 'male'
+}
+
+const sayHi = (person : Human):string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
+}
+console.log(sayHi(person));
 
 export {};
